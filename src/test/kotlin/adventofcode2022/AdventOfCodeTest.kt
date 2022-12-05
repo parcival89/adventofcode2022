96d9b3618811aebaf0beda0ef1d83c988dc5053c
Sander Paulus <sander@Sanders-MacBook-Pro.local>
2022-12-05 16:30:29 +0100

    Day 4 - rest of the commit

--- src/test/kotlin/adventofcode2022/AdventOfCodeTest.kt
+++ src/test/kotlin/adventofcode2022/AdventOfCodeTest.kt
@@ -80,6 +80,50 @@ class AdventOfCodeTest {
         println(sum)
     }
 
+    @Test
+    fun `Day4 - Count cleaning overlap`() {
+        val overlappingAssignments = readFile("day4.txt")
+            .map { it.split(",") }
+            .count(::checkOverlap)
+        println(overlappingAssignments)
+    }
+
+    @Test
+    fun `Day4 - Count cleaning partial overlap`() {
+        val overlappingAssignments = readFile("day4.txt")
+            .map { it.split(",") }
+            .count(::checkAnyOverlap)
+
+        println(overlappingAssignments)
+    }
+
+    private fun checkOverlap(sections: List<String>): Boolean {
+        val first = splitBoundaries(sections.first())
+        val last = splitBoundaries(sections.last())
+        return overlapsCompletely(first, last) || overlapsCompletely(last, first)
+    }
+
+    private fun checkAnyOverlap(sections: List<String>): Boolean {
+        val first = splitBoundaries(sections.first())
+        val last = splitBoundaries(sections.last())
+        return !( hasNoOverlap(first, last) && hasNoOverlap(last, first))
+    }
+
+    private fun overlapsCompletely(overlapper: Pair<Int, Int>, overlappee: Pair<Int, Int>): Boolean {
+        return overlappee.first >= overlapper.first && overlappee.second <= overlapper.second
+    }
+
+    private fun splitBoundaries(sections: String): Pair<Int, Int> {
+        val boundaries =  sections.split("-").map { it.toInt() }
+        return boundaries.first() to boundaries.last()
+    }
+
+    // 29-29, 31-80
+    private fun hasNoOverlap(first: Pair<Int, Int>, second: Pair<Int, Int>): Boolean {
+        return (first.second < second.first && first.second < second.second) ||
+                (first.first > second.first && first.first > second.second)
+    }
+
     fun readFile(fileName: String): List<String> =
         {}::class.java.classLoader.getResourceAsStream(fileName)?.reader()?.readLines()
             ?: error("Could not load $fileName")
